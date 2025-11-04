7dd74ebcdc50cea39c9b48fce6964dafe2f365d7
shahriar0999 <shahriar0999@proton.me>
2025-11-04 17:33:00 +0600

    load dataset from huggingFace

---
+++ 1.working-with-hugging-face/load_dataset.py
@@ -0,0 +1,7 @@
+from datasets import load_dataset
+
+# Load the "validation" split of the TIGER-Lab/MMLU-Pro dataset
+my_dataset = load_dataset("TIGER-Lab/MMLU-Pro", split="validation")
+
+# Display dataset details
+print(my_dataset)
