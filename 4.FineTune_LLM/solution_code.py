69282ad3efe513060fcbb8dc44b62b5c57bcbb23
shahriar0999 <shahriar0999@proton.me>
2025-11-06 21:56:46 +0600

    Tokenizing function

--- 4.FineTune_LLM/solution_code.py
+++ 4.FineTune_LLM/solution_code.py
@@ -9,3 +9,30 @@ tokenized_training_data = tokenizer(train_data["interaction"], return_tensors="p
 tokenized_test_data = tokenizer(test_data["interaction"], return_tensors="pt", padding=True, truncation=True, max_length=20)
 
 print(tokenized_training_data)
+
+
+
+# Complete the function
+def tokenize_function(data):
+    return tokenizer(data["interaction"], 
+                     return_tensors='pt', 
+                     padding=True, 
+                     truncation=True, 
+                     max_length=64)
+
+tokenized_in_batches = train_data.map(tokenize_function, batched=True)
+
+
+
+# Complete the function
+def tokenize_function(data):
+    return tokenizer(data["interaction"], 
+                     return_tensors="pt", 
+                     padding=True, 
+                     truncation=True, 
+                     max_length=64)
+
+# Tokenize row by row
+tokenized_by_row = train_data.map(tokenize_function, batched=False)
+
+print(tokenized_by_row)
