405efd580267ed70c0ba976029830284da8a176b
shahriar0999 <shahriar0999@proton.me>
2025-11-06 22:08:34 +0600

    Evaluating with METEOR

--- 5.Evaluate_finetune_model/loading_metrics_with_evaluate.py
+++ 5.Evaluate_finetune_model/loading_metrics_with_evaluate.py
@@ -135,3 +135,19 @@ references = ["""Pluto is a dwarf planet in the solar system, located in the Kui
 # Calculate the rouge scores between the predicted and reference summaries
 results = rouge.compute(predictions=predictions, references=references)
 print("ROUGE results: ", results)
+
+
+"""
+Evaluating with METEOR
+METEOR excels at evaluating some of the more semantic features in text. It works similar to ROUGE by comparing a model-generated output to a reference output. You've been provided these texts as generated and reference; it's over to you to evaluate the score.
+
+The evaluate library has been loaded for you."""
+
+meteor = evaluate.load("meteor")
+
+generated = ["The burrow stretched forward like a narrow corridor for a while, then plunged abruptly downward, so quickly that Alice had no chance to stop herself before she was tumbling into an extremely deep shaft."]
+reference = ["The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly that Alice had not a moment to think about stopping herself before she found herself falling down a very deep well."]
+
+# Compute and print the METEOR score
+results = meteor.compute(predictions=generated, references=reference)
+print("Meteor: ", results['meteor'])
