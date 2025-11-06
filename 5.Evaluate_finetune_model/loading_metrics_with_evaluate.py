11785f760c00f58720e77e5c26c67972ec70db86
shahriar0999 <shahriar0999@proton.me>
2025-11-06 22:10:58 +0600

    Checking toxicity

--- 5.Evaluate_finetune_model/loading_metrics_with_evaluate.py
+++ 5.Evaluate_finetune_model/loading_metrics_with_evaluate.py
@@ -168,3 +168,32 @@ references = ["What a wonderful day", "I love cats", "DataCamp has great AI cour
 # Compute the exact match and print the results
 results = exact_match.compute(predictions=predictions, references=references)
 print("EM results: ", results)
+
+
+"""
+Checking toxicity
+You work at a social media company and need to evaluate the comments that are being left on the platform to ensure users are respecting the code of conduct.
+
+Your task is to carefully assess the toxicity level of comments of user_1 and user_2.
+
+The toxicity metric has been loaded for you as toxicity_metric.
+"""
+
+# Calculate the individual toxicities
+toxicity_1 = toxicity_metric.compute(predictions=user_1)
+toxicity_2 = toxicity_metric.compute(predictions=user_2)
+print("Toxicities (user_1):", toxicity_1['toxicity'])
+print("Toxicities (user_2): ", toxicity_2['toxicity'])
+
+# Calculate the maximum toxicities
+toxicity_1_max = toxicity_metric.compute(predictions=user_1, aggregation='maximum')
+toxicity_2_max = toxicity_metric.compute(predictions=user_2, aggregation='maximum')
+print("Maximum toxicity (user_1):", toxicity_1_max['max_toxicity'])
+print("Maximum toxicity (user_2): ", toxicity_2_max['max_toxicity'])
+
+# Calculate the toxicity ratios
+toxicity_1_ratio = toxicity_metric.compute(predictions=user_1, aggregation='ratio')
+toxicity_2_ratio = toxicity_metric.compute(predictions=user_2, aggregation='ratio')
+print("Toxicity ratio (user_1):", toxicity_1_ratio['toxicity_ratio'])
+print("Toxicity ratio (user_2): ", toxicity_2_ratio['toxicity_ratio'])
+
