18a4b7a4a14128d660b9a530fae94cdaa3d36cdc
shahriar0999 <shahriar0999@proton.me>
2025-11-06 22:02:10 +0600

    Describing metrics

--- 5.Evaluate_finetune_model/loading_metrics_with_evaluate.py
+++ 5.Evaluate_finetune_model/loading_metrics_with_evaluate.py
@@ -3,3 +3,23 @@ accuracy = evaluate.load("accuracy")
 precision = evaluate.load('precision')
 recall = evaluate.load('recall')
 f1 = evaluate.load('f1')
+
+
+"""
+Describing metrics
+It's never a bad time to revise the definitions of some popular metrics.
+
+The evaluate library has been loaded for you, along with the four classification metrics: accuracy, precision, recall, and f1.
+"""
+# Obtain a description of each metric
+print(accuracy.description)
+print(precision.description)
+print(recall.description)
+print(f1.description)
+
+
+# See the required data types
+print(f"The required data types for accuracy are: {accuracy.features}.")
+print(f"The required data types for precision are: {precision.features}.")
+print(f"The required data types for recall are: {recall.features}.")
+print(f"The required data types for f1 are: {f1.features}.")
