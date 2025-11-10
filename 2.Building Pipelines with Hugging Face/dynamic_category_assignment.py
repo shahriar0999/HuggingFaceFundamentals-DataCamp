b90ba32143c0b5b42c45bbd00f59f08cb0c56e45
shahriar0999 <shahriar0999@proton.me>
2025-11-10 23:56:58 +0600

    dynamic category

---
+++ 2.Building Pipelines with Hugging Face/dynamic_category_assignment.py
@@ -0,0 +1,13 @@
+text = "AI-powered robots assist in complex brain surgeries with precision."
+
+# Create the pipeline
+classifier = pipeline(task="zero-shot-classification", model="facebook/bart-large-mnli")
+
+# Create the categories list
+categories = ["politics", "science", "sports"]
+
+# Predict the output
+output = classifier(text, categories)
+
+# Print the top label and its score
+print(f"Top Label: {output['labels'][0]} with score: {output['scores'][0]}")
